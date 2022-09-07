db5613a90f0eb11523ebedcd169b6f9120eaf918
JuliethPalacios <110795477+JuliethPalacios@users.noreply.github.com>
2022-09-07 14:20:00 -0500

    Add files via upload

---
+++ ejemplo anaconda.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+numeros=[1,2,3,4,5,6,7]
+
+arreglo=[10,2,"hola mundo",1,5]
+
+letras=['a','b','c','d','A',]
+
+arregloslice=arreglo[1:-1]
+
+letras.sort()
+
+arregloextendido=numeros
+
+arregloextendido.extend(letras)
+
+print(letras)
+
+letras.sort(key=str.lower)
+
+letras
+
+letras2=['a','n','b','C','A','z','D','Z']
+
+letras2.sort(key=str.lower)
+
+letras2
+
